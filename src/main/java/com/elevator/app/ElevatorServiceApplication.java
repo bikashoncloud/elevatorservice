1efe67713423fbed2f5656479c80b96a75fe5703
Bikash Kumar <bikakuma@in.ibm.com>
2023-05-09 20:04:29 +0530

    first commit

---
+++ src/main/java/com/elevator/app/ElevatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elevator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorServiceApplication.class, args);
+	}
+
+}
